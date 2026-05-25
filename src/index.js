48023ec7735f81a74fb3e08b81e1a82f0b9b46f6
Rhuan Kowic <dev.rhuankowic@gmail.com>
2026-05-24 21:53:00 -0300

    feat(cli): alterando index para simulacao de fluxo e teste de erro.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,46 @@
-console.log("--- ProcEngine CLI ---");
+import { Process, priorityProcess } from "./model/Process.js";
+import { StatusProcess } from "./model/History.js";
+
+console.log("=== INICIANDO PROCENGINE CLI ===");
+
+try {
+  const processServer = new Process(
+    "Instabilidade no servidor",
+    priorityProcess.ALTA,
+  );
+  console.log(`\nProcesso criado com sucesso!`);
+  console.log(`ID: ${processServer.id}`);
+  console.log(`Título: ${processServer.title}`);
+  console.log(`Status: ${processServer.status}`);
+  console.log(`Prioridade: ${processServer.priority}`);
+  console.log(`Data de Criação: ${processServer.creationDate}`);
+  console.log(`Data Limite: ${processServer.limitDate}`);
+
+  console.log("\n--------------------------------------------------");
+  processServer.changeStatus(
+    StatusProcess.EM_ANDAMENTO,
+    "Análise iniciada pela equipe de Infraestrutura.",
+  );
+  console.log(`Status atualizado para: ${processServer.status}`);
+
+  processServer.changeStatus(
+    StatusProcess.CONCLUÍDO,
+    "Problema resolvido e monitoramento concluído.",
+  );
+  console.log(`Status atualizado para: ${processServer.status}`);
+
+  console.log("\n------------------ HISTÓRICO DE STATUS -----------------");
+  processServer.listHistory.forEach((log, index) => {
+    console.log(`[Registro: ${index + 1}] ${log.toString()}`);
+  });
+  console.log("\n--------------------------------------------------");
+  console.log(`Teste de erro: Tentando atualizar para o mesmo status...`);
+  processServer.changeStatus(
+    StatusProcess.CONCLUÍDO,
+    "Tentativa de atualização para o mesmo status.",
+  );
+} catch (error) {
+  {
+    console.error(`Erro: ${error.message}`);
+  }
+}
